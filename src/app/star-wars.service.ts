c6d226ec54346303a9921c19b664c664e138a901
Tall Mamadou Lamine <mtall1432@gmail.com>
2020-03-30 18:40:07 +0200

    First commit

---
+++ src/app/star-wars.service.ts
@@ -0,0 +1,48 @@
+import { LogService } from './log.service';
+import { Injectable } from "@angular/core";
+@Injectable()
+export class StarWarsService{
+  private characters = [
+    {
+      name: 'Luke Skywalker',
+      side: '',
+    },
+    {
+      name: 'Darkwader',
+      side: '',
+    }
+  ]
+  private logService : LogService;
+  constructor(logService: LogService){
+    this.logService =logService;
+
+  }
+  getCharacters(chosenList){
+    if(chosenList === 'all'){
+    return this.characters.slice()
+    }
+    return this.characters.filter((char)=>{
+      return char.side === chosenList;
+    }
+    )
+
+  }
+  onSideChosen(charInfo){
+    const pos = this.characters.findIndex((char)=>{
+      return char.name === charInfo.name
+    })
+    this.characters[pos].side = charInfo.side;
+    this.logService.writeLog('changed side of '+ charInfo.name +', new side: '+ charInfo.side);
+   }
+   addCharacter(name, side){
+    const pos = this.characters.findIndex((char)=>{
+      return char.name === name
+    })
+    if (pos !== -1){
+      return;
+    }
+
+     const newChar = {name:name, side:side}
+     this.characters.push(newChar)
+   }
+}
