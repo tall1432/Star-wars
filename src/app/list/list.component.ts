ba09071eb8afb940909a91e8b390a5b1ce75479e
Tall Mamadou Lamine <mtall1432@gmail.com>
2020-03-31 13:33:18 +0200

    ajout de nav-bar

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit,Input  } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-list',
@@ -6,12 +8,22 @@ import { Component, OnInit,Input  } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-   @Input() characters;
+   characters = [] ;
+   activatedRoutes:ActivatedRoute;
+   swService: StarWarsService;
 
 
-  constructor() { }
+  constructor(activatedRoutes: ActivatedRoute, swService:StarWarsService) {
+    this.activatedRoutes = activatedRoutes;
+    this.swService= swService;
+  }
 
   ngOnInit(): void {
+    this.activatedRoutes.params.subscribe(
+      (params)=>{
+this.characters = this.swService.getCharacters(params.side)
+      }
+    )
   }
 
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,28 +1,17 @@
 import { Component, OnInit } from '@angular/core'
-import { StarWarsService } from '../star-wars.service';
+
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css'],
-  
+
 })
 export class TabsComponent implements OnInit {
-  characters=[];
-  chosenList = 'all';
-  swService: StarWarsService;
-  constructor(swService: StarWarsService) {
-    this.swService = swService;
+
+  constructor() {
+
    }
 
   ngOnInit(): void {}
-  onChoose(side){
-    this.chosenList = side;
   }
-getCharacters(){
-
-  this.characters = this.swService.getCharacters(this.chosenList);
- return this.characters;
-}
-
-}
